1b5e85976a1315f7542c857b1909e2d3f7d0ec76
DrNickRedfern <71695111+DrNickRedfern@users.noreply.github.com>
2020-09-24 16:12:40 +0100

    Upload HSM3.R

---
+++ HSM3.R
@@ -0,0 +1,19 @@
+HSM3 <- function(x){ # x is an image file
+  
+  r <- R(x) * 255; g <- G(x) * 255; b <- B(x) * 255 
+  
+  rg <- r - g
+  yb <- 0.5 * (r + g) - b
+  
+  m.rg <- mean(rg); m.yb <- mean(yb)
+  s.rg <- sd(rg); s.yb <- sd(yb)
+  
+  m.rgyb <- sqrt( (m.rg^2) + (m.yb^2) )
+  
+  s.rgyb <- sqrt( (s.rg^2) + (s.yb^2) )
+  
+  h <- s.rgyb + (0.3 * m.rgyb)
+  
+  return(h)
+  
+}
